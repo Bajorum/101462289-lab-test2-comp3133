82038580b6d4b88d5e85c27781f093e1710da909
Bilal Youssef <156091600+Bajorum@users.noreply.github.com>
2025-04-02 23:03:29 -0400

    Update app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { ViewChild } from '@angular/core';
 import { MissionlistComponent } from './missionlist/missionlist.component';
 import { MissionfilterComponent } from './missionfilter/missionfilter.component';
-
 
 @Component({
   selector: 'app-root',
@@ -12,13 +10,14 @@ import { MissionfilterComponent } from './missionfilter/missionfilter.component'
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-
 export class AppComponent {
   title = '101472085-lab-test2-comp3133';
 
   @ViewChild('missionList') missionListComponent!: MissionlistComponent;
 
-  onYearSelected(year: string) {
-    this.missionListComponent.getMissionsByYear(year);
+  onFiltersChanged(filters: any) {
+    if (this.missionListComponent) {
+      this.missionListComponent.getMissions(filters);
+    }
   }
 }
